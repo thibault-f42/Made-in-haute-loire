17b0ffba7503e07a2890a8e95ff8447a5b455c34
Shindounet <79092612+Shindounet@users.noreply.github.com>
2021-05-19 14:01:16 +0200

    entités et relations

---
+++ src/Controller/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+{
+
+
+    /**
+     * @Route ("/", name = "Accueil")
+     */
+    public function home ()  {
+
+        return $this->render('Accueil.html.twig')  ;
+    }
+
+
+}
---
+++ src/Entity/Photo.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PhotoRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PhotoRepository::class)
+ */
+class Photo
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $urlPhoto;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Entreprise::class, inversedBy="photos")
+     */
+    private $entreprise;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Produit::class, inversedBy="photos")
+     */
+    private $produit;
+
+    public function __construct()
+    {
+        $this->entreprise = new ArrayCollection();
+        $this->produit = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUrlPhoto(): ?string
+    {
+        return $this->urlPhoto;
+    }
+
+    public function setUrlPhoto(string $urlPhoto): self
+    {
+        $this->urlPhoto = $urlPhoto;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Entreprise[]
+     */
+    public function getEntreprise(): Collection
+    {
+        return $this->entreprise;
+    }
+
+    public function addEntreprise(Entreprise $entreprise): self
+    {
+        if (!$this->entreprise->contains($entreprise)) {
+            $this->entreprise[] = $entreprise;
+        }
+
+        return $this;
+    }
+
+    public function removeEntreprise(Entreprise $entreprise): self
+    {
+        $this->entreprise->removeElement($entreprise);
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Produit[]
+     */
+    public function getProduit(): Collection
+    {
+        return $this->produit;
+    }
+
+    public function addProduit(Produit $produit): self
+    {
+        if (!$this->produit->contains($produit)) {
+            $this->produit[] = $produit;
+        }
+
+        return $this;
+    }
+
+    public function removeProduit(Produit $produit): self
+    {
+        $this->produit->removeElement($produit);
+
+        return $this;
+    }
+}
