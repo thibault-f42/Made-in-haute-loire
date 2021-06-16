bcfdbffde328eea8cd018da5e01cd0ffddc579c3
DimitriB26 <76944667+DimitriB26@users.noreply.github.com>
2021-06-16 11:26:07 +0200

    Revert "Revert "Mercredi Matin 2""
    This reverts commit 294381e254b5a965be45afaeda130912cf3e253d.

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -9,6 +9,7 @@ use App\Form\ProduitType;
 use App\Repository\EntrepriseRepository;
 use App\Repository\FichierRepository;
 use App\Repository\ProduitRepository;
+use App\Repository\SousCategorieRepository;
 use App\Repository\UtilisateurRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -65,7 +66,7 @@ class ProduitController extends AbstractController
      * @Route("/ajout-produit", name="AjoutProduit")
      * @return Response
      */
-    public function ajoutProduit(UtilisateurRepository $utilisateurRepository, Request $request): Response
+    public function ajoutProduit(UtilisateurRepository $utilisateurRepository, Request $request, SousCategorieRepository $sousCategorieRepository): Response
     {
 
         $utilisateur= $utilisateurRepository->find($this->getUser());
@@ -143,7 +144,20 @@ class ProduitController extends AbstractController
             $entityManager->persist($ajoutProduit);
             $entityManager->flush();
 
-            return $this->redirectToRoute('produitPartenaire');
+            return $this->redirectToRoute('produitPartenaire', ['id'=>$entreprise->getId()]);
+        }
+
+
+
+        //ajax
+        if ($request->get('ajax') && $request->get('produit')['categorie']) {
+
+            $categorieString = $request->get('produit')['categorie'];
+            $categorie = (int)$categorieString;
+            $sousCategories = $sousCategorieRepository->getSousCategorieByCategorieIdAjax($categorie);
+            return new JsonResponse([
+                'content' => $this->renderView('categorie/_selectCategorie.html.twig', compact('sousCategories'))
+            ]);
         }
 
         return $this->render('produit/AjoutProduit.html.twig', [
